cd06b5a839ea2c3c30d1e0d3f26923758d34303f
sonia sebastian <sonia@easicloud.ca>
2024-07-23 11:10:16 -0400

    added outputs and changes made

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -11,14 +11,10 @@ resource "aws_s3_bucket" "my_s3_bucket" {
     }
 }
 
+# STATE  
+# DESIRED-KNOWN-ACTUAL 
 
-output "my_s3_bucket_versioning" {
-  value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
-}
 
-output "my_s3_bucket_complete_details" {
-  value = aws_s3_bucket.my_s3_bucket
-}
 
 resource "aws_iam_user" "my_iam_user" {
   name = "my_iam_user_abc_updated"
