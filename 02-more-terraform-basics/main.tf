a8826f37fa019005153066d53b4ddeb1c27eeb9a
sonia sebastian <sonia@easicloud.ca>
2024-07-23 17:23:50 -0400

    added files

--- 02-more-terraform-basics/main.tf
+++ 02-more-terraform-basics/main.tf
@@ -1,9 +1,28 @@
+variable "iam_user_name_prefix" {
+  type    = string #any, number, bool, list, map, set, object, tuple
+  default = "my_iam_user"
+}
+
 provider "aws" {
-    region = "us-east-1"
-    version ="~> 2.46"
-    }
+  region  = "us-east-1"
+  version = "~> 2.46"
+}
 
 resource "aws_iam_user" "my_iam_users" {
-    count = 2
-    name = "my_iam_user_${count.index}"
+  count = 3
+  name  = "${var.iam_user_name_prefix}_${count.index}"
 }
+
+
+
+# COMMANDS
+
+# terraform plan -0 iam.tfplan
+# terraform apply iam.tfplan
+# terraform validate
+# terraform fmt
+# terraform console
+# terraform show
+# -target= 
+# -auto-approve
+
