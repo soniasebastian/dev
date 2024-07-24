98523d763ebe23209278ff847ab44468820031d7
sonia sebastian <sonia@easicloud.ca>
2024-07-24 15:29:10 -0400

    added changes

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -1,3 +1,8 @@
+variable "aws_key_pair" {
+  default = "~/aws/aws_keys/default_ec2.pem"
+}
+
+
 provider "aws" {
   region  = "us-east-1"
   version = "~> 2.46"
@@ -7,6 +12,8 @@ provider "aws" {
 #SG-> 80 TCP, 22 TCP, CIDR ["0.0.0.0/0]
 
 # plan - execute 
+
+
 resource "aws_security_group" "http_server_sg" {
   name   = "http_server_sg"
   vpc_id = "vpc-038edb66ebd0b0b3a"
@@ -37,6 +44,31 @@ resource "aws_security_group" "http_server_sg" {
     name = "http_server_sg"
   }
 }
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-0427090fd1714168b"
+  key_name               = "default_ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = "subnet-0461969d25c77cb1f"
+
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",                                                                                  //install httpd 
+      "sudo service httpd start",                                                                                   //start
+      "echo Welcome to my DevOps learning - Virtual Server is at ${self.public_dns} | sudo tee /var/www/index.html" //copy a file
+    ]
+  }
+}
+
 
 
 
